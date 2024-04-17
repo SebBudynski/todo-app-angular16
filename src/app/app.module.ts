5859b10a7a29ed0938d5e303e9b30f45a57c59a2
Sebastian Budyński <Sebbudynski@gmail.com>
2024-04-17 21:16:46 +0200

    Saving todos in local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-input/todo-input.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.css'],
+})
+export class TodoInputComponent {
+  todo = '';
+
+  addTodo() {
+    if (this.todo.trim() === '') {
+      return;
+    }
+
+    const todos = JSON.parse(localStorage.getItem('todos') || '[]');
+    todos.push(this.todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    this.todo = '';
+    console.log('Działa');
+  }
+}
